942005a71aa690bc60a4d6a37ee3f240ea0ac570
BogdanRybak1996 <bogdanrybak1996@gmail.com>
2016-04-05 13:40:18 +0300

    InitialCommit

---
+++ app/src/main/java/com/bogdanrybak1996/fivenewfilms/MainActivity.java
@@ -0,0 +1,13 @@
+package com.bogdanrybak1996.fivenewfilms;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
